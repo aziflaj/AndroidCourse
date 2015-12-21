52c4930d73de664d76e58002e89e3f362511566b
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-12-21 20:21:28 +0100

    BasicPhrases plays sound when button is clicked

--- BasicPhrases/app/src/main/java/com/aziflaj/basicphrases/MainActivity.java
+++ BasicPhrases/app/src/main/java/com/aziflaj/basicphrases/MainActivity.java
@@ -1,13 +1,63 @@
 package com.aziflaj.basicphrases;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playSound(View view) {
+        int viewId = view.getId();
+        MediaPlayer mediaPlayer;
+
+        switch (viewId) {
+            case R.id.hello:
+                mediaPlayer = MediaPlayer.create(this, R.raw.hello);
+                mediaPlayer.start();
+                break;
+
+            case R.id.how_are_you:
+                mediaPlayer = MediaPlayer.create(this, R.raw.howareyou);
+                mediaPlayer.start();
+                break;
+
+            case R.id.speak_english:
+                mediaPlayer = MediaPlayer.create(this, R.raw.doyouspeakenglish);
+                mediaPlayer.start();
+                break;
+
+            case R.id.evening:
+                mediaPlayer = MediaPlayer.create(this, R.raw.goodevening);
+                mediaPlayer.start();
+                break;
+
+            case R.id.live:
+                mediaPlayer = MediaPlayer.create(this, R.raw.ilivein);
+                mediaPlayer.start();
+                break;
+
+            case R.id.name:
+                mediaPlayer = MediaPlayer.create(this, R.raw.mynameis);
+                mediaPlayer.start();
+                break;
+
+            case R.id.please:
+                mediaPlayer = MediaPlayer.create(this, R.raw.please);
+                mediaPlayer.start();
+                break;
+
+            case R.id.welcome:
+                mediaPlayer = MediaPlayer.create(this, R.raw.welcome);
+                mediaPlayer.start();
+                break;
+        }
     }
 }
