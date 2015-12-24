b57c749545637838b0e59dffc6ac02b96360ba89
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-12-24 20:40:37 +0100

    randomly chooses a celebrity

--- GuesstheCelebrity/app/src/main/java/com/aziflaj/guessthecelebrity/MainActivity.java
+++ GuesstheCelebrity/app/src/main/java/com/aziflaj/guessthecelebrity/MainActivity.java
@@ -1,15 +1,20 @@
 package com.aziflaj.guessthecelebrity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String CELEBRITIES_URL = "http://www.posh24.com/celebrities";
+
     ImageView celebrityImageView;
     Button answer1;
     Button answer2;
@@ -31,12 +36,25 @@ public class MainActivity extends AppCompatActivity {
 
         FetchCelebritiesTask fetchCelebritiesTask = new FetchCelebritiesTask();
         try {
-            Log.i("Getting celebrities", "start");
-            celebrityMap = fetchCelebritiesTask.execute("http://www.posh24.com/celebrities").get();
+            celebrityMap = fetchCelebritiesTask.execute(CELEBRITIES_URL).get();
+
+            Random random = new Random();
+            int rnd = random.nextInt(celebrityMap.size());
+
+            String randomCelebrity = (String) celebrityMap.keySet().toArray()[rnd];
+            Log.i("MainActivity", randomCelebrity);
+
+            Bitmap bmp = new ImageDownloadTask().execute(celebrityMap.get(randomCelebrity)).get();
+            celebrityImageView.setImageBitmap(bmp);
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+    }
+
+    public void answer(View view) {
+
     }
 }
