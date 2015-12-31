b405f3fe7568bfd9fa9ab2954f81ad3e2e63a74b
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-12-31 12:37:09 +0100

    initial commit: notes

---
+++ Notes/app/src/main/java/com/aziflaj/notes/MainActivity.java
@@ -0,0 +1,33 @@
+package com.aziflaj.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                // add the item
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
