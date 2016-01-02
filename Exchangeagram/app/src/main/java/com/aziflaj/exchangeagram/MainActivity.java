ef33674f5e6977edea67fea8690777457e1dbfcc
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-01-02 13:00:29 +0100

    Login and Signup functionality using Parse

--- Exchangeagram/app/src/main/java/com/aziflaj/exchangeagram/MainActivity.java
+++ Exchangeagram/app/src/main/java/com/aziflaj/exchangeagram/MainActivity.java
@@ -2,66 +2,88 @@ package com.aziflaj.exchangeagram;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.parse.FindCallback;
+import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-
-import java.util.List;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private EditText mUsernameEditText;
+    private EditText mPasswordEditText;
+    private Button mSessionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-        // Object creation
-        ParseObject progressObject = new ParseObject("Progress");
-        progressObject.put("username", "aziflaj");
-        progressObject.put("progress", 50);
-        progressObject.saveInBackground();
-        //*/
-
-        /*
-        // Object update
-        ParseQuery<ParseObject> progressQuery = ParseQuery.getQuery("Progress");
-        progressQuery.getInBackground("9fgdj9XNY8", new GetCallback<ParseObject>() {
-            @Override
-            public void done(ParseObject object, ParseException e) {
-                if (e == null) {
-                    object.put("progress", 56);
-                    object.saveInBackground();
-                    Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
-                } else {
-                    e.printStackTrace();
-                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        //*/
-
-        //*
-        // get all objects
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Progress");
-        query.whereEqualTo("username", "aziflaj");
-        query.setLimit(1); // username should be unique
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-                if (e == null && objects.size() > 0) {
-                    ParseObject aziflaj = objects.get(0);
-                    aziflaj.put("progress", 58);
-                    aziflaj.saveInBackground();
-                }
-            }
-        });
-        //*/
-
+        mUsernameEditText = (EditText) findViewById(R.id.username_edit_text);
+        mPasswordEditText = (EditText) findViewById(R.id.password_edit_text);
+        mSessionButton = (Button) findViewById(R.id.session_button);
 
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+
+    public void handleSession(View view) {
+        String username = mUsernameEditText.getText().toString();
+        String password = mPasswordEditText.getText().toString();
+
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Username or password is empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (view.getTag().toString().equals("login")) {
+            ParseUser.logInInBackground(username, password, new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if (user != null) {
+                        Toast.makeText(getApplicationContext(), "Logged in", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+            ParseUser user = new ParseUser();
+            user.setUsername(username);
+            user.setPassword(password);
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        Toast.makeText(getApplicationContext(), "Signed up", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    public void switchSession(View view) {
+        String sessionTag = mSessionButton.getTag().toString();
+        TextView self = (TextView) view;
+
+        if (sessionTag.equals("login")) {
+            // make signup
+            mSessionButton.setText(R.string.session_button_signup);
+            self.setText(R.string.session_text_login);
+            mSessionButton.setTag("signup");
+        } else {
+            mSessionButton.setText(R.string.session_button_login);
+            self.setText(R.string.session_text_signup);
+            mSessionButton.setTag("login");
+        }
     }
 }
