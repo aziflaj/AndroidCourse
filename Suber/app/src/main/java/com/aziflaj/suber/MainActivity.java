91ab087da4a832e25a152f7d00175391f5df7f20
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-01-05 20:59:11 +0100

    anonymous signup activity layout

--- Suber/app/src/main/java/com/aziflaj/suber/MainActivity.java
+++ Suber/app/src/main/java/com/aziflaj/suber/MainActivity.java
@@ -3,37 +3,31 @@ package com.aziflaj.suber;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Switch;
 import android.widget.Toast;
 
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.SaveCallback;
+import com.parse.ParseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch mSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button) findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ParseObject testObject = new ParseObject("TestObject");
-                testObject.put("foo", "bar");
-                testObject.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if (e == null) {
-                            Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(getApplicationContext(), "Lesht", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
-        });
+        mSwitch = (Switch) findViewById(R.id.rider_driver_switch);
+
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+
+    public void signup(View view) {
+        if (mSwitch.isChecked()) {
+            Toast.makeText(getApplicationContext(), "Driver", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Rider", Toast.LENGTH_SHORT).show();
+        }
     }
 }
