8ddcc9f64fe366c393404374ca331d5e3f9fb139
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-03-10 22:31:54 +0100

    show top/bottom tubes

--- Flippy Bird/core/src/com/aziflaj/flippybird/FlippyBird.java
+++ Flippy Bird/core/src/com/aziflaj/flippybird/FlippyBird.java
@@ -7,8 +7,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlippyBird extends ApplicationAdapter {
     SpriteBatch batch;
-    Texture flippy;
+    Texture[] flippy;
     Texture background;
+    Texture topTube;
+    Texture bottomTube;
+
+    int birdState = 0;
 
     float birdY = 0f; // bird's position
     float velocity = 0;
@@ -19,10 +23,12 @@ public class FlippyBird extends ApplicationAdapter {
     @Override
     public void create() {
         batch = new SpriteBatch();
-        flippy = new Texture("flippy.png");
+        flippy = new Texture[]{new Texture("bird.png"), new Texture("bird2.png")};
         background = new Texture("bg.png");
+        topTube = new Texture("toptube.png");
+        bottomTube = new Texture("bottomtube.png");
 
-        birdY = Gdx.graphics.getHeight() / 2 - flippy.getHeight() / 2;
+        birdY = Gdx.graphics.getHeight() / 2 - flippy[0].getHeight() / 2;
     }
 
     @Override
@@ -37,23 +43,32 @@ public class FlippyBird extends ApplicationAdapter {
             }
 
             if (birdY - velocity > Gdx.graphics.getHeight()) {
-                birdY = Gdx.graphics.getHeight() + flippy.getHeight();
+                birdY = Gdx.graphics.getHeight() - flippy[0].getHeight();
             }
 
             if (birdY > 0 || velocity < 0) {
                 velocity += gravity;
                 birdY -= velocity;
             }
+            birdState = birdState == 1 ? 0 : 1;
         } else {
-
             if (Gdx.input.justTouched()) {
                 gameState = 1;
+                velocity = -30;
             }
         }
 
         batch.begin();
         batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.draw(flippy, Gdx.graphics.getWidth() / 2 - flippy.getWidth() / 2, birdY);
+        batch.draw(flippy[birdState],
+                (Gdx.graphics.getWidth() - flippy[birdState].getWidth()) / 2,
+                birdY);
+        batch.draw(topTube,
+                (Gdx.graphics.getWidth() - topTube.getWidth()) / 2,
+                Gdx.graphics.getHeight() - topTube.getHeight() / 2);
+        batch.draw(bottomTube,
+                (Gdx.graphics.getWidth() - bottomTube.getWidth()) / 2,
+                -bottomTube.getHeight() / 2);
         batch.end();
     }
 }
